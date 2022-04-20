81bd76cbcf0b66e4e8cc8c38e655821749430faf
kushkurl <kushagra.kurl@gmail.com>
2022-04-20 17:07:33 -0400

    added user identity model and next step is to configure navigation on user loggin status

---
+++ Areas/Identity/Pages/DashBoard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FreeShare.Areas.Identity.Pages
+{
+    public class DashBoardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
